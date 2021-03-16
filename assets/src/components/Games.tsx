2163cc3181f153e02e7870335a6e80d00a5d3408
marissamanley <58527286+marissamanley@users.noreply.github.com>
2021-03-16 14:35:58 -0400

    Added Sidebar and Header to Games page

--- assets/src/components/Games.tsx
+++ assets/src/components/Games.tsx
@@ -1,9 +1,27 @@
 import React, { useState, useEffect } from 'react'
+import './Games.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
 
-function Games() {
+interface GamesProps {}
+
+function Games({}: GamesProps) {
     return (
-        <h1>Games</h1>
+        <div className="app">
+            <div className="games-content">
+                <Header/>
+                <div className="container">
+                    <Sidebar/>
+                    <div className="title">
+                        <h1>Game Catalog</h1>
+                    </div>
+                    <div className="games">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
     )
-}
+}   
 
 export default Games
