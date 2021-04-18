a4af8c23d02a0acaff48ae9ffd24c6103b16f2cb
Ethan Haengel <haengele@ufl.edu>
2021-04-18 14:37:19 -0400

    Groups page now gets information from the database.

--- assets/src/components/Groups/Groups.tsx
+++ assets/src/components/Groups/Groups.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import GroupCard from './GroupCard'
 
 function Groups() {
+    const [groups, setGroups] = useState([])
+
+    useEffect(() => {
+      fetch('/api/groups')
+        .then(response => response.json())
+        .then(evnts => {
+          setGroups(evnts)
+          console.log(evnts)
+        })
+    }, [])
   return (
     <main>
-      <GroupCard />
-      <GroupCard />
-      <GroupCard />
+      {
+      groups.map((group: any) => (
+        <GroupCard
+          key={group.title}
+          name={group.name}
+          game={group.game}
+          url={group.url}
+          description={group.description}
+        />
+      ))
+    }
     </main>
   )
 }
